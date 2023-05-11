356e1ae06d6b88f4e2278c5df157d7ee314a922d
Lord Tachanka <92011080+OmoTachanka@users.noreply.github.com>
2023-05-11 12:05:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ async def predict(request: Request, background_tasks: BackgroundTasks, file: Upl
         content = await file.read()
         myfile.write(content)
         myfile.close()
-    print(type(file.filename))
+    
     results = utils.superres(imagename = file.filename)
     background_tasks.add_task(cleanup, PATH_FILES, file.filename)
     return templates.TemplateResponse("superres.html", {"request": request, "results": results})
